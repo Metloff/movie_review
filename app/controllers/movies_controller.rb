cf2995705d96b9b4e00b71fd27c25760ece25efd
Henry <genrih.metlov@gmail.com>
2015-12-30 08:03:26 +0000

    Review scaffold

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -48,7 +48,7 @@ class MoviesController < ApplicationController
   def destroy
     @movie.destroy
     respond_to do |format|
-      format.html { redirect_to movies_url, notice: 'Movie was successfully destroyed.' }
+      format.html { redirect_to root_url, notice: 'Movie was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
